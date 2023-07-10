e66ffac6f082ee2ea048c84f052386d9359ee742
includeVitor <vitorzcomputacao@gmail.com>
2023-07-10 11:20:42 +0100

    add TodoWeb.ItemsLive

---
+++ lib/todo_web/live/items_live.ex
@@ -0,0 +1,14 @@
+defmodule TodoWeb.ItemsLive do
+  use TodoWeb, :live_view
+
+  alias Todo.Items
+  alias Todo.Items.Item
+
+  def mount(_param, _session, socket) do
+    {:ok, assign(socket, items: Items.list_items()) |> assign_items()}
+  end
+
+  def assign_items(socket) do
+    socket |> assign(:items, %Item{})
+  end
+end
