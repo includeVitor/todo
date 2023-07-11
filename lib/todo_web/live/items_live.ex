ec282d9a8998196169409335588525fa7d6348b9
includeVitor <vitorzcomputacao@gmail.com>
2023-07-11 12:51:14 +0100

    add handle_info -> items_updated

--- lib/todo_web/live/items_live.ex
+++ lib/todo_web/live/items_live.ex
@@ -4,11 +4,19 @@ defmodule TodoWeb.ItemsLive do
   alias Todo.Items
   alias Todo.Items.Item
 
+  @items_topic "items"
+
   def mount(_param, _session, socket) do
+    if connected?(socket), do: TodoWeb.Endpoint.subscribe(@items_topic)
+
     {:ok, assign(socket, items: Items.list_items()) |> assign_items()}
   end
 
   def assign_items(socket) do
     socket |> assign(:item, %Item{})
+  end
+
+  def handle_info(%{event: "items_updated", payload: %{items: items}}, socket) do
+    {:noreply, assign(socket, items: items)}
   end
 end
