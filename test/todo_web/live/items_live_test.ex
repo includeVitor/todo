fa740112edd6a1f9f0918b79d51baf84799ac9a5
includeVitor <vitorzcomputacao@gmail.com>
2023-07-12 13:04:21 +0100

    getting started with the tests

---
+++ test/todo_web/live/items_live_test.ex
@@ -0,0 +1,21 @@
+defmodule TodoWeb.ItemsLiveTest do
+  use TodoWeb.ConnCase
+  import Phoenix.LiveViewTest
+
+  @valid_item_attrs %{text: "first_item"}
+  @invalid_item_attrs %{text: nil}
+
+  test "create_item", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/")
+
+    # assert view
+    #        |> form("#item-form", item: @invalid_item_attrs)
+    #        |> render_change() =~ "can&#39;t be blank"
+
+    view
+    |> form("#item-form", item: @valid_item_attrs)
+    |> render_submit()
+
+    assert render(view) =~ "first item"
+  end
+end
