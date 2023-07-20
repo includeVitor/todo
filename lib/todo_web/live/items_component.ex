ded801a0280181d0004b8c2bbdef0102061feb6c
includeVitor <vitorzcomputacao@gmail.com>
2023-07-20 11:19:12 +0100

    handling toggle_item and item_completed

--- lib/todo_web/live/items_component.ex
+++ lib/todo_web/live/items_component.ex
@@ -1,4 +1,20 @@
 defmodule TodoWeb.ItemsComponent do
   use TodoWeb, :live_component
   use Phoenix.HTML
+  alias Todo.Items
+
+  @items_topic "items"
+
+  def handle_event("toggle_item", %{"id" => id}, socket) do
+    item = Items.get_item!(id)
+    Items.update_item(item, %{completed: !item.completed})
+
+    socket = assign(socket, items: Items.list_items())
+    TodoWeb.Endpoint.broadcast(@items_topic, "items_updated", socket.assigns)
+    {:noreply, socket}
+  end
+
+  @spec item_completed?(any) :: <<_::_*72>>
+  def item_completed?(true), do: "completed"
+  def item_completed?(_), do: ""
 end
