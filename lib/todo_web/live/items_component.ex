0be596ce4ea4e7539407bfc1cdc0b07d5b166b93
includeVitor <vitorzcomputacao@gmail.com>
2023-07-20 11:57:56 +0100

    add handle_event delete_item

--- lib/todo_web/live/items_component.ex
+++ lib/todo_web/live/items_component.ex
@@ -14,6 +14,15 @@ defmodule TodoWeb.ItemsComponent do
     {:noreply, socket}
   end
 
+  def handle_event("delete_item", %{"id" => id}, socket) do
+    item = Items.get_item!(id)
+    Items.delete_item(item)
+
+    socket = assign(socket, items: Items.list_items())
+    TodoWeb.Endpoint.broadcast(@items_topic, "items_updated", socket.assigns)
+    {:noreply, socket}
+  end
+
   @spec item_completed?(any) :: <<_::_*72>>
   def item_completed?(true), do: "completed"
   def item_completed?(_), do: ""
