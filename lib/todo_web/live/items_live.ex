e5e6e46ad4f89591ce6653c46d8572892dc739c2
includeVitor <vitorzcomputacao@gmail.com>
2023-07-21 11:30:50 +0100

    using  @impl true cleanup

--- lib/todo_web/live/items_live.ex
+++ lib/todo_web/live/items_live.ex
@@ -5,34 +5,35 @@ defmodule TodoWeb.ItemsLive do
 
   @items_topic "items"
 
+  @impl true
   def mount(_param, _session, socket) do
     if connected?(socket), do: TodoWeb.Endpoint.subscribe(@items_topic)
 
     {:ok, assign(socket, items: Items.list_items()) |> assign_items()}
   end
 
-  def assign_items(socket) do
-    socket |> assign(:item, %Item{})
-  end
-
+  @impl true
   def handle_info(%{event: "items_updated", payload: %{items: items}}, socket) do
     {:noreply, assign(socket, items: items)}
   end
 
+  @impl true
   def handle_params(params, _url, socket) do
     items = Items.list_items()
 
     case params["filter_by"] do
       "completed" ->
-        completed_items = Enum.filter(items, &(&1.completed == true))
-        {:noreply, assign(socket, items: completed_items)}
+        {:noreply, assign(socket, items: Enum.filter(items, &(&1.completed == true)))}
 
       "active" ->
-        active_items = Enum.filter(items, &(&1.completed == false))
-        {:noreply, assign(socket, items: active_items)}
+        {:noreply, assign(socket, items: Enum.filter(items, &(&1.completed == false)))}
 
       _ ->
         {:noreply, assign(socket, items: items)}
     end
+  end
+
+  def assign_items(socket) do
+    socket |> assign(:item, %Item{})
   end
 end
