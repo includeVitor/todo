8f5a222c6c3d56f6c7f8b12d5ba0661bf9f30fc9
includeVitor <vitorzcomputacao@gmail.com>
2023-07-21 12:19:47 +0100

    add is_filter_by_selected

--- lib/todo_web/live/filter_component.ex
+++ lib/todo_web/live/filter_component.ex
@@ -1,4 +1,14 @@
 defmodule TodoWeb.FilterComponent do
   use TodoWeb, :live_component
   use Phoenix.HTML
+
+  def is_filter_by_selected?(filter_by, label) do
+    case filter_by == label do
+      true ->
+        "bg-blue-100"
+
+      _ ->
+        ""
+    end
+  end
 end
