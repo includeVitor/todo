666c11cb06352f7f6ffcd93c56e09200e3a8260c
includeVitor <vitorzcomputacao@gmail.com>
2023-07-22 20:41:33 +0100

    add excoveralls

--- mix.exs
+++ mix.exs
@@ -9,7 +9,14 @@ defmodule Todo.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.details": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -49,7 +56,8 @@ defmodule Todo.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
